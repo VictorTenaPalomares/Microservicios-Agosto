6b4f5d644fb6e4f3cee87f2aff446d830acaff70
Victor Tena Palomares <victor.tena@viewnext.com>
2021-07-30 15:02:10 +0200

    Se ha terminado el proyecto 03

--- .metadata/.plugins/org.eclipse.core.resources/.history/99/0009a5060df1001b1c5acb97f4f6e6d0
+++
@@ -1,8 +0,0 @@
-package com.boot.ejemplo;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class SaludoController {
-
-}
--- 03_microservicio_mapeado_objetos_CRUD/src/test/java/com/boot/mapeando/ApplicationTests.java
+++ 03_microservicio_mapeado_objetos_CRUD/src/test/java/com/boot/mapeando/ApplicationTests.java
@@ -1,13 +1,49 @@
 package com.boot.mapeando;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@AutoConfigureMockMvc // dejamos que spring autoconfigure el objeto para las pruebas
 @SpringBootTest
 class ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// Para hacer las pruebas de los recursos que expone nuestro servicio
+	// usaremos el objeto MockMVC el cual lo anotaremos con autowired
+	@Autowired
+	MockMvc mock; //
 
+	// A continuación escribimos nuestras pruebas.En este caso se va a lanzar una 
+	// petición http de tipo get con la dirección dameCursos cuyo resultado nos lo va a imprimir en consola
+	@Test
+	void testCursos() {
+		try {
+			mock.perform(get("/dameCursos")).andDo(print());
+		} catch (Exception e) {
+			System.out.println("Ha ocurrido un probelma de tipo: " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	
+	
+	// Ahora probamos el método delete, al ser una petición HTTP que no se puede
+	// realizar vía navegador, esta es la forma óptima de probar que funciona 
+	// correctamente
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
